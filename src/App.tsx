aa3319e729fdd9771e5a8f412c0a846acbad4042
Florian Janke <t-fljan@microsoft.com>
2020-08-12 16:11:10 -0500

    Edit topology

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export const App: React.FunctionComponent<IProps> = (props) => {
         };
     });
 
-    const editingTopology = false;
+    const editingTopology = true;
 
     const graph = new Graph();
 
