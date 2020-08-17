be0f49e3731c54a9188a44ee3e5b9960d107cd33
Florian Janke <t-fljan@microsoft.com>
2020-08-17 14:21:10 -0500

    Combine functions

--- src/editor/components/ParameterPanel.tsx
+++ src/editor/components/ParameterPanel.tsx
@@ -48,21 +48,16 @@ const GraphPanelEditField: React.FunctionComponent<IGraphPanelEditFieldProps> = 
 
     const onChange = (event: React.FormEvent, newValue?: string) => {
         if (newValue !== undefined) {
+            let error = "";
+            if (!newValue) {
+                error = Localizer.l("sidebarGraphInstanceParameterMissing");
+            }
+
+            // TODO: Perform additional validation
+
+            setParameter({ ...parameter, error, value: newValue });
             setValue(newValue);
-            setParameter({ ...parameter, value: newValue });
-            validateInput(newValue);
         }
-    };
-
-    const validateInput = (newValue: string) => {
-        let error = "";
-        if (!newValue) {
-            error = Localizer.l("sidebarGraphInstanceParameterMissing");
-        }
-
-        // TODO: Perform additional validation
-
-        setParameter({ ...parameter, error, value: newValue });
     };
 
     return (
